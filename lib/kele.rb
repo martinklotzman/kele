376fa2120f95f71feea9204b66383868d5d5483e
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-02-11 13:13:39 -0800

    get_messages method to retrieve messages for current user and create_message method to create new message and thread

--- lib/kele.rb
+++ lib/kele.rb
@@ -29,4 +29,14 @@ include Roadmap
     response = self.class.get('https://www.bloc.io/api/v1/mentors/2400043/student_availability', headers: { "authorization": @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_messages(page)
+    response = self.class.get('https://www.bloc.io/api/v1/message_threads', body: { "page": page }, headers: { "authorization": @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, token, subject, stripped_text)
+    response = self.class.post('https://www.bloc.io/api/v1/messages', body: { "sender": sender, "recipient_id": recipient_id, "token": token, "subject": subject, "stripped-text": stripped_text }, headers: { "authorization": @auth_token })
+    JSON.parse(response.body)
+  end
 end
