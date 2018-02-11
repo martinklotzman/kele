d3b19efcfb4e2b495adb16d2fa0e8c573d507683
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-02-10 16:28:44 -0800

    retrieve mentors available slot times and convert them to ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,7 +19,13 @@ include HTTParty
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization": @auth_token })
     @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    @mentor_id = mentor_id
+    response = self.class.get('https://www.bloc.io/api/v1/mentors/2400043/student_availability', headers: { "authorization": @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
 end
