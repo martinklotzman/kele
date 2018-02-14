64bc12c64a2521dfa200864d02fc25941640a630
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-02-13 18:10:58 -0800

    cleanup roadmap.rb

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -8,13 +8,13 @@ include HTTParty
 
   def get_roadmap(roadmap_id)
     #example 37
-    response = self.class.get('https://www.bloc.io/api/v1/roadmaps/' + roadmap_id.to_s, headers: { "authorization": @auth_token })
-    @roadmap = JSON.parse(response.body)
+    response = self.class.get("https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}", headers: { "authorization": @auth_token })
+    JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
     #example 2227
-    response = self.class.get('https://www.bloc.io/api/v1/checkpoints/' + checkpoint_id.to_s, headers: { "authorization": @auth_token })
-    @checkpoint = JSON.parse(response.body)
+    response = self.class.get("https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}", headers: { "authorization": @auth_token })
+    JSON.parse(response.body)
   end
 end
